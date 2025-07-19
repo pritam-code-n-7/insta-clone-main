7a3cf678e8ab4a9312f44fd3d5029e41dfc8ef42
Pritam Nandy <pritamimca1003@gmail.com>
2025-07-19 20:39:48 +0530

    work (project): * Card ui fixed - height, width adjusted * now uploaded image size is maximum 2mb

--- demo/body-demo/Body.tsx
+++ demo/body-demo/Body.tsx
@@ -51,21 +51,21 @@ const Body = async () => {
         <ScrollArea className="border p-5 rounded-xl h-[500px] bg-muted/50">
           <div className="grid lg:grid-cols-4 grid-cols-1 gap-2">
           {posts.map((post) => (
-            <Card key={post._id} className="bg-blue-50 border-0 shadow-md py-0 w-full lg:h-[450px] h-[400px]">
+            <Card key={post._id} className="bg-blue-50 border-0 shadow-md py-0 w-[300px] lg:h-[450px] h-[400px]">
                 {post.image?.secure_url && (
                   <Image
                     src={post.image.secure_url}
                     alt={post.caption}
                     width={200}
                     height={200}
-                    className="w-full rounded-t-xl "
+                    className="w-[300px] h-[200px] rounded-t-xl "
                   />
                 )}
-              <CardContent className="dark:text-black mb-2">
+              <CardContent className="dark:text-black mb-2 h-24">
                 <QuoteIcon className="rotate-180 right-0"/>
                 <p className="font-semibold text-pretty lg:text-base text-xs ml-4">{post.caption}</p>
               </CardContent>
-              <CardFooter className="flex items-center justify-between">
+              <CardFooter className="flex items-center justify-between h-24">
                 <Link href={`/edit/${post._id}`}>
                 <ButtonDemo name="Edit" type="button" className="h-12 w-24 rounded-md shadow-md bg-black"/>
                 </Link>
--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 };
 
 export default nextConfig;
